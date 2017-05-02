994a541ef8f1742e288157ba82ce2758026d8fc2
Mark Corner <mcorner@eduroamguest-2-197.sys.cs.local>
2017-05-02 15:09:11 -0400

    moving more into the library

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-const SyncResults = require('./sync_results');
+const SyncResults = require('lib/sync_results');
 module.exports.SyncResults = SyncResults.SyncResults;
+
+const BufferedSensor = require('lib/buffered_sensor');
+module.exports.BufferedSensor  = BufferedSensor.BufferedSensor ;
