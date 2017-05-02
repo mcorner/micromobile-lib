f48f6b99591d3621155124acc3defb849551f12f
Mark Corner <mcorner@eduroamguest-2-197.sys.cs.local>
2017-05-02 15:13:40 -0400

    paths

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const SyncResults = require('lib/sync_results');
+const SyncResults = require('./lib/sync_results');
 module.exports.SyncResults = SyncResults.SyncResults;
 
-const BufferedSensor = require('lib/buffered_sensor');
+const BufferedSensor = require('./lib/buffered_sensor');
 module.exports.BufferedSensor  = BufferedSensor.BufferedSensor ;
