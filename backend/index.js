d08abfcc5b0e7f393d18465bbfc8fe0ac5e556d1
Mark Corner <mcorner@MacBook-Pro-2.local>
2017-05-09 14:59:34 -0400

    debug info

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ const s3 = new AWS.S3();
 const config = require('./config.json');
 
 exports.experimentFileResultsHandler = function(event, context, callback) {
+  console.log(event);
+  console.log(event.data);
+  console.log(JSON.stringify(event));
   const param = {Bucket: config.backend_bucket, Key: 'experiment-results/' + event.experiment_name, Body: event.data};
 
   s3.upload(param, function(err, data) {
