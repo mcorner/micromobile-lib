a25badfcbee9f1cb43d5ba489ff1e79520acc12a
Mark Corner <mcorner@MacBook-Pro-2.local>
2017-05-08 21:42:14 -0400

    adding sensor events, untested

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const SyncResults = require('./lib/sync_results');
 module.exports.SyncResults = SyncResults.SyncResults;
 
 const BufferedSensor = require('./lib/buffered_sensor');
-module.exports.BufferedSensor  = BufferedSensor.BufferedSensor ;
+module.exports.BufferedSensor  = BufferedSensor.BufferedSensor;
+
+const SensorEvent = require('./lib/sensor_event');
+module.exports.SensorEvent = SensorEvent.SensorEvent;
+module.exports.SENSOR_TYPES = SensorEvent.SENSOR_TYPES;
